1129e9681263fbdca5b9f00d1bf89c7dbbdb906f
adelguit <adeelguitoun@gmail.com>
2022-10-03 14:40:41 +0100

    Copy project files from the last Todo-List project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import _ from 'lodash';
+import * as base from './modules/base';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+// Select DOM Elements
+const form = document.forms[0];
+const taskInput = form.task;
+const todoList = document.querySelector('.todo_body_list');
+const clearBtn = document.querySelector('.clear');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+base.createTask(taskInput, todoList, form);
+base.deleteTask(todoList);
+base.editTask(todoList);
+base.loadTasks(todoList);
+base.checkTask(todoList);
+base.clearTask(todoList, clearBtn);
